0fae5a62770ec13027b5a04bd6effdf6bcf7b650
VishalV-GIT <vishalvallamrao@gmail.com>
2024-11-29 11:15:25 +0530

    Menu Navigation System.cpp

--- Menu Navigation System.cpp
+++ Menu Navigation System.cpp
@@ -3,64 +3,57 @@
 using namespace std;
 
 int main() {
-    int currentMenu = 0; 
-    
-    while (true) {
-        if (currentMenu == 0) {  
-            cout << "\nMain Menu\n";
-            cout << "1. Settings\n";
-            cout << "2. Media\n";
-            cout << "3. Exit\n";
-            cout << "Enter Comamnd: ";
+    int currentmenu=0; 
+    while(true) {
+        if (currentmenu == 0) {  
+            cout << "\nMain menu\n";
+            cout <<"1.Settings\n";
+            cout <<"2.Media\n";
+            cout <<"3.Exit\n";
+            cout <<"Enter Comamnd: ";
             int choice;
             cin >> choice;
-
-            if (choice == 1) {
-                currentMenu = 1;  
-            } else if (choice == 2) {
-                currentMenu = 2;  
-            } else if (choice == 3) {
+            if (choice==1) {
+                currentmenu=1;  
+            } else if(choice==2) {
+                currentmenu=2;  
+            } else if(choice==3) {
                 break; 
             } else {
-                cout << "Invalid Command\n";
-            }
-        } 
-        else if (currentMenu == 1) {  
-            cout << "\nSettings Menu\n";
-            cout << "1. Display Settings\n";
-            cout << "2. Audio Settings\n";
-            cout << "3. Back to Main Menu\n";
-            cout << "Enter Command: ";
+                cout << "Invalid command\n";}} 
+        else if(currentmenu==1) {  
+            cout << "\nSettings menu\n";
+            cout <<"1.Displaysettings\n";
+            cout <<"2.Audiosettings\n";
+            cout <<"3.Back to main menu\n";
+            cout <<"Enter Command: ";
+            int choice;
+            cin>>choice;
+
+            if(choice==1) {
+                cout<<"Displaysettings\n";
+            }else if(choice==2) {
+                cout << "Audiosettings\n";
+            }else if (choice==3) {
+                currentmenu=0;  
+            } else {
+                cout << "Invalid Command\n";}} 
+        else if(currentmenu == 2) {  
+            cout<<"\nMedia menu\n";
+            cout<<"1.Radio\n";
+            cout<<"2. Bluetooth audio\n";
+            cout<<"3. Back to Main menu\n";
+            cout<<"Enter Command: ";
             int choice;
             cin >> choice;
-
-            if (choice == 1) {
-                cout << "Display Settings\n";
-            } else if (choice == 2) {
-                cout << "Audio Settings\n";
-            } else if (choice == 3) {
-                currentMenu = 0;  
-            } else {
-                cout << "Invalid Command\n";
-            }
-        } 
-        else if (currentMenu == 2) {  
-            cout << "\nMedia Menu\n";
-            cout << "1. Radio\n";
-            cout << "2. Bluetooth Audio\n";
-            cout << "3. Back to Main Menu\n";
-            cout << "Enter Command: ";
-            int choice;
-            cin >> choice;
-
-            if (choice == 1) {
-                cout << "Radio\n";
-            } else if (choice == 2) {
-                cout << "Bluetooth Audio\n";
-            } else if (choice == 3) {
-                currentMenu = 0;  
-            } else {
-                cout << "Enter Command:\n";
+            if(choice==1) {
+                cout<<"Radio\n";
+            }else if(choice==2){
+                cout<<"Bluetooth Audio\n";
+            }else if(choice==3){
+                currentmenu=0;  
+            }else{
+                cout <<"Enter Command:\n";
             }
         }
     }
